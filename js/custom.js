200aa34a198deb0e5327d24554aad3a5192a73fe
sdx <sdfddgfxfg@gmail.com>
2022-03-24 22:52:58 +0530

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,1 @@
+$(window).on('load',function(){$('.preloader').delay(1000).fadeOut("slow");});$(function(){jQuery(document).ready(function(){$('body').backstretch(["images/tm-bg-slide-1.jpg",],{duration:3200,fade:1300});});})
